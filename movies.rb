af59c6321e1982f4a58995e8be194d7c98ddb970
Jarques <hello@jpretorius.com>
2011-11-23 12:36:49 -0800

    Added a reply based on rating. Next up will be random replies for each rating.

--- movies.rb
+++ movies.rb
@@ -6,7 +6,7 @@ require 'open-uri'
 class Movies < SiriPlugin
 
   def initialize()
-    @api_key = "ROTTEN TOMATOES API KEY"    
+    @api_key = "ROTTEN TOMATOES API KEY"
   end
 
   def object_from_guzzoni(object, connection)     
@@ -21,19 +21,33 @@ class Movies < SiriPlugin
     if(phrase.match(/what is the rating of (.+)/i))
       self.plugin_manager.block_rest_of_session_from_server
 
-      term = $1.strip
+      search_term = $1.strip
 
-      url = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?q=#{term}&page_limit=1&page=1&apikey=#{@api_key}"
+      url = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?q=#{search_term}&page_limit=1&page=1&apikey=#{@api_key}"
       results = JSON.parse(open(url).read)
+      
+      reply = self.construct_reply(results)
 
-      # Need to check if any results have been returned.
-
-      audience_score = results["movies"][0]["ratings"]["audience_score"]
-
-      return generate_siri_utterance(connection.lastRefId, "Overall audience score is #{audience_score}")
+      return generate_siri_utterance(connection.lastRefId, reply)
     end
 
     object
+  end
+  
+  def construct_reply(results)
+    user_rating = results["movies"][0]["ratings"]["audience_score"]
+    title       = results["movies"][0]["title"]
+    
+    if user_rating < 50
+      "It looks like #{title} didn't do so well, #{user_rating}%"
+    elsif user_rating < 70
+      "#{title} looks okay, a score of #{user_rating}%"
+    elsif user_rating < 80
+      "Not bad, #{title} received #{user_rating}%"
+    else
+      "Great, #{title} got #{user_rating}%. I'll get the popcorn."
+    end
+    
   end
 
 end
